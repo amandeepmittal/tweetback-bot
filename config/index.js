f05cdafae6db51269b69497cab75698b42353d70
amandeepmittal <amandeepmittal@live.com>
2017-01-30 23:43:29 +0530

    refactor: add config

---
+++ config/index.js
@@ -0,0 +1,13 @@
+/*
+ * TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key       : 'mgi5l1JG6JeId6QxhisST9Jh8',
+    consumer_secret    : '4xn9vz7sb57rIgSAQK3QFUGZ5DHwE6ZhAifq29P4cV1D0xWdIB',
+    access_token       : '1419909222-iHFGnVTSFmtnxG9UcaQCuhjVqhurCoPHkMrPOdA',
+    access_token_secret: '5LFMC7KZRxdBQKrUbZJ5bAOvT3Jn066ZD4axKzopQLw8c'
+};
