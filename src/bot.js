3fa0f7a9bd1fb0fc38c055cda919d942c004b294
amandeepmittal <amandeepmittal@live.com>
2017-03-31 10:56:49 +0530

    only once with cron job

--- src/bot.js
+++ src/bot.js
@@ -1,72 +1,65 @@
 'use strict';
 
-
 // Dependencies
-const config = require('../config/index');
-const articles = require('./articles');
-const Twit =  require('twit');
-const uniqueRandomArray = require('unique-random-array');
-const stoicapi = require('stoic-api').random();
+const config = require('../config/index')
+const articles = require('./articles')
+const Twit =  require('twit')
+const uniqueRandomArray = require('unique-random-array')
+const stoicapi = require('stoic-api').random()
+const schedule = require('node-schedule')
 
-const T = new Twit(config);
+const T = new Twit(config)
 
 // indicate the bot is running
-console.log('Tweet-back Bot is runnning');
+console.log('Tweet-back Bot is runnning')
 
 /**
  * Tweet Technical Articles
  */
 function tweetTechArticles () {
-    let techArticles = uniqueRandomArray(articles.tech);
+    let techArticles = uniqueRandomArray(articles.tech)
     let message = techArticles();
 
     T.post('statuses/update', {status: message}, (err, data, response) => {
-        if (err) {
-            console.error('CANNOT SEND TWEET' + err);
-        }
-        console.log('TWEETED SUCCESSFULLY: Technical Article');
-    });
+        if (err) console.error('CANNOT SEND TWEET' + err)
+        console.log('TWEETED SUCCESSFULLY: Technical Article')
+    })
 }
 
 // tweet randomly and in every 12 hours
-setInterval(tweetTechArticles, 1000 * 60 * 60 * 12);
+setInterval(tweetTechArticles, 1000 * 60 * 60 * 12)
 
 /**
  * Tweet Other Medium Stories
  */
 
 function tweetOtherMediumArticles () {
-    let otherMediumArticles = uniqueRandomArray(articles.otherMediumStories);
-    let message = otherMediumArticles();
+    let otherMediumArticles = uniqueRandomArray(articles.otherMediumStories)
+    let message = otherMediumArticles()
 
     T.post('statuses/update', {status: message}, (err, data, response) => {
-        if (err) {
-            console.error('CANNOT SEND TWEET' + err);
-        }
-        console.log('TWEETED SUCCESSFULLY: Other Medium Article');
-    });
+        if (err) console.error('CANNOT SEND TWEET' + err)
+        console.log('TWEETED SUCCESSFULLY: Other Medium Article')
+    })
 }
 // tweet randomly and in every 12 hours
-setInterval(tweetOtherMediumArticles, 1000 * 60 * 60 * 12);
+setInterval(tweetOtherMediumArticles, 1000 * 60 * 60 * 12)
 
 
 /*
  * Tweet these articles once every three days
  */
+schedule.scheduleJob('* 22 * * 5', () => {
+  let otherMediumArticles = uniqueRandomArray(articles.onlyOnce)
+  let message = otherMediumArticles()
 
- function onlyOnce () {
-     let otherMediumArticles = uniqueRandomArray(articles.onlyOnce);
-     let message = onlyOnce();
-
-     T.post('statuses/update', {status: message}, (err, data, response) => {
-         if (err) {
-             console.error('CANNOT SEND TWEET' + err);
-         }
-         console.log('TWEETED SUCCESSFULLY: Only Once Article');
-     });
- }
- // tweet randomly and in every 2 days
- setInterval(onlyOnce, 1000 * 60 * 60 * 24 * 2);
+  T.post('statuses/update', {status: message}, (err, data, response) => {
+    if (err) {
+      console.error('CANNOT SEND TWEET' + err)
+    }
+    console.log('TWEETED SUCCESSFULLY: Only Once Article')
+  })
+})
 
 /**
  * TODO: Tweet Book Blog posts
@@ -81,12 +74,10 @@ function tweetDailyStoic () {
     let message = stoicapi;
 
     T.post('statuses/update', {status:message}, (err, data, response) => {
-        if (err) {
-            console.error('CANNOT SEND TWEET' + err);
-        }
-        console.log('TWEETED SUCCESSFULLY: Daily Stoic Quote');
+        if (err) console.error('CANNOT SEND TWEET' + err)
+        console.log('TWEETED SUCCESSFULLY: Daily Stoic Quote')
     });
 }
 
 // tweet randomly and in every 24 hours
-setInterval(tweetDailyStoic, 1000 * 60 * 60 * 24);
+setInterval(tweetDailyStoic, 1000 * 60 * 60 * 24)
