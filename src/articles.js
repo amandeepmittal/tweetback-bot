431497f5b3f663f8b849486b25b81787f65c54ab
amandeepmittal <amandeepmittal@live.com>
2017-08-11 10:24:51 +0530

    fix links of articles

--- src/articles.js
+++ src/articles.js
@@ -6,23 +6,15 @@ module.exports = {
         '#Tutorial: Writing an Advanced #Twitter #Bot using #Nodejs & #JavaScript https://community.risingstack.com/how-to-make-a-twitter-bot-with-node-js/ via @RisingStack #Tech',
         'How to make a #Twitter #Bot with #Nodejs & #JavaScript https://chatbotslife.com/how-to-make-a-twitter-bot-with-nodejs-d5cb04fdbf97#.c4bzt4slr via @ChatBotsLife #Tech',
         'Tip: How to use #Bower with #Heroku to manage your #FrontEnd #JavaScript Dependencies https://medium.com/@amanhimself/using-bower-with-heroku-cdc791320c88#.ria1rs44c #Tech',
-        'How #Nodejs works? An overview https://medium.com/@amanhimself/the-node-js-system-51090c35dddc#.ddl1s583z #Tech',
+        'How #Nodejs works? An overview https://hackernoon.com/the-node-js-system-51090c35dddc #Tech',
         'The #Nodejs way, #philosophy of the #JavaScript platform https://medium.com/@amanhimself/the-node-way-philosophy-of-a-platform-f9738ed5f9d2#.upgfhgvut #Tech',
         'Migrating from #Expressjs 4 to 5 https://hackernoon.com/migrating-from-expressjs-4-to-5-156dcd80eb11#.blwzfpr42 via @hackernoon #JavaScript #Nodejs',
         'How to Mock #Data for #Nodejs #Applications using #Faker.js https://hackernoon.com/how-to-mock-data-for-node-js-applications-using-faker-js-b1f4c0e78102 #JavaScript via @hackernoon',
-        'How to Write #Modules in #Angularjs? https://amandeepmittal.github.io/blog/2017/04/14/Modules-in-Angularjs/ via @amanhimself #JavaScript #Tech #WebDevelopment',
         'Getting Started with #IonicFramework https://hackernoon.com/getting-started-with-ionic-framework-an-overview-6725b687779b#.yvj6rnijh #Hybrid #overview #Mobile',
         'How to use #Eslint in #Nodejs Applications? https://hackernoon.com/how-to-use-eslint-in-node-js-applications-cc4b2298ce55 via @hackernoon #JavaScript',
-        'Modular #programming in #Angularjs https://amandeepmittal.github.io/blog/2017/05/18/Modular-programming-in-Angularjs/ via @amanhimself #JavaScript #Tech #WebDevelopment',
-        'Concepts in #Angularjs https://amandeepmittal.github.io/blog/2017/05/21/Concepts-in-Angularjs/ #JavaScript #WebDevelopment',
         'Getting Started with #Sequelize for #Nodejs Applications https://hackernoon.com/getting-started-with-sequelize-for-nodejs-applications-2854c58ffb8c #JavaScript #MySQL',
-        'What\'s new in #npm 5? https://amandeepmittal.github.io/blog/2017/06/02/What-s-new-in-npm-5 #nodejs #javascript #packagemanager #development #tools',
-        'How to Remove #Nodejs Modules Recurisvely https://amandeepmittal.github.io/blog/2017/06/06/Remove-node-modules-recursively #JavaScript #npm #devtips',
-        'Tip: How to use #Bower with #Heroku to manage your #FrontEnd #JavaScript Dependencies https://amandeepmittal.github.io/blog/2017/06/10/Using-Bower-with-Heroku/ #Tech',
-        'Getting Started with #Sequelize for #Nodejs Applications https://amandeepmittal.github.io/blog/2017/05/30/Getting-Started-with-Sequelize-for-Nodejs-Applications #JavaScript #MySQL',
-        'Tip: Setting up #MeanStack with #Webstorm https://amandeepmittal.github.io/blog/2017/05/05/Setting-up-MEAN-Stack-with-Webstorm/ #nodejs #javascript #development',
-        'How to use #Eslint in #Nodejs Applications? https://amandeepmittal.github.io/blog/2017/04/28/Eslint-with-Nodejs/ #JavaScript',
+        'Tip: Setting up #MeanStack with #Webstorm https://medium.com/@amanhimself/setting-up-mean-stack-with-webstorm-5024d3c650ed #nodejs #javascript #development',
         'How To Maintain #Sanity When Finding A #Job https://42hire.com/how-to-maintain-sanity-when-finding-a-job-87d368bcfd0c#.jintkr98n via @42hire #Career #Productivity #Tech #jobhunting #jobsearch',
-        'How to make #TwitterBots with #Nodejs https://amandeepmittal.github.io/blog/2017/06/15/How-to-make-a-Twitter-Bot-with-NodeJS/ #JavaScript'
+        'Converting a Buffer to JSON and Utf8 Strings in #Nodejs https://hackernoon.com/https-medium-com-amanhimself-converting-a-buffer-to-json-and-utf8-strings-in-nodejs-2150b1e3de57 #JavaScript #Tech'
     ]
 };
