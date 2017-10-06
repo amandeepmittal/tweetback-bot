6d189d367aaf2e24e5bb9b6a918e969e5ad954f6
amandeepmittal <amandeepmittal@live.com>
2017-10-06 17:58:25 +0530

    add new article link

--- src/articles.js
+++ src/articles.js
@@ -18,6 +18,7 @@ module.exports = {
 		'Converting a Buffer to JSON and Utf8 Strings in #Nodejs https://hackernoon.com/https-medium-com-amanhimself-converting-a-buffer-to-json-and-utf8-strings-in-nodejs-2150b1e3de57 #JavaScript #Tech',
 		'How to Hack Your Mornings so You Get Most of Your Time https://artplusmarketing.com/how-to-hack-your-mornings-so-you-get-most-of-your-time-b3e4d39c5b7c #LifeLessons #MorningThoughts',
 		'Setup Nodemon to auto restart #Nodejs #app server  https://hackernoon.com/setup-nodemon-to-auto-restart-nodejs-application-server-8d8993b7dfd9',
-		'Remove #Node Modules Recursively #nodejs #npm https://medium.com/@amanhimself/https-medium-com-amanhimself-remove-node-modules-recursively-5b5dd5fcc9ba'
+		'Remove #Node Modules Recursively #nodejs #npm https://medium.com/@amanhimself/https-medium-com-amanhimself-remove-node-modules-recursively-5b5dd5fcc9ba',
+		'Introduction to Hybrid #Mobile #Apps https://hackernoon.com/introduction-to-hybrid-mobile-apps-c97720b30557 #ionic #IonicFramework #javascript'
 	]
 };
