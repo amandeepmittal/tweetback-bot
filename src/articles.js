bd40ed43d96036876250d9c5db3baf71963f1e61
amandeepmittal <amandeepmittal@live.com>
2017-11-19 13:02:14 +0530

    v1.8.0: add 3 new blog article links

--- src/articles.js
+++ src/articles.js
@@ -1,26 +1,29 @@
 module.exports = {
-	// NOTE: Tehcnical blog posts
-	tech: [
-		'Learn to build your own #Twitter #Bot in #Nodejs #JavaScript https://hackernoon.com/create-a-simple-twitter-bot-with-node-js-5b14eb006c08#.90wbpgb8e via @hackernoon #Tech',
-		'#Tutorial: Writing a #Nodejs #Twitter #Bot https://community.risingstack.com/node-js-twitter-bot-tutorial/ via @RisingStack #JavaScript #Tech ',
-		'#Tutorial: Writing an Advanced #Twitter #Bot using #Nodejs & #JavaScript https://community.risingstack.com/how-to-make-a-twitter-bot-with-node-js/ via @RisingStack #Tech',
-		'How to make a #Twitter #Bot with #Nodejs & #JavaScript https://chatbotslife.com/how-to-make-a-twitter-bot-with-nodejs-d5cb04fdbf97#.c4bzt4slr via @ChatBotsLife #Tech',
-		'Tip: How to use #Bower with #Heroku to manage your #FrontEnd #JavaScript Dependencies https://medium.com/@amanhimself/using-bower-with-heroku-cdc791320c88#.ria1rs44c #Tech',
-		'How #Nodejs works? An overview https://hackernoon.com/the-node-js-system-51090c35dddc #Tech',
-		'The #Nodejs way, #philosophy of the #JavaScript platform https://medium.com/@amanhimself/the-node-way-philosophy-of-a-platform-f9738ed5f9d2#.upgfhgvut #Tech',
-		'Migrating from #Expressjs 4 to 5 https://hackernoon.com/migrating-from-expressjs-4-to-5-156dcd80eb11#.blwzfpr42 via @hackernoon #JavaScript #Nodejs',
-		'How to Mock #Data for #Nodejs #Applications using #Faker.js https://hackernoon.com/how-to-mock-data-for-node-js-applications-using-faker-js-b1f4c0e78102 #JavaScript via @hackernoon',
-		'Getting Started with #IonicFramework https://hackernoon.com/getting-started-with-ionic-framework-an-overview-6725b687779b#.yvj6rnijh #Hybrid #overview #Mobile',
-		'How to use #Eslint in #Nodejs Applications? https://hackernoon.com/how-to-use-eslint-in-node-js-applications-cc4b2298ce55 via @hackernoon #JavaScript',
-		'Getting Started with #Sequelize for #Nodejs Applications https://hackernoon.com/getting-started-with-sequelize-for-nodejs-applications-2854c58ffb8c #JavaScript #MySQL',
-		'Tip: Setting up #MeanStack with #Webstorm https://medium.com/@amanhimself/setting-up-mean-stack-with-webstorm-5024d3c650ed #nodejs #javascript #development',
-		'How To Maintain #Sanity When Finding A #Job https://42hire.com/how-to-maintain-sanity-when-finding-a-job-87d368bcfd0c#.jintkr98n via @42hire #Career #Productivity #Tech #jobhunting #jobsearch',
-		'Converting a Buffer to JSON and Utf8 Strings in #Nodejs https://hackernoon.com/https-medium-com-amanhimself-converting-a-buffer-to-json-and-utf8-strings-in-nodejs-2150b1e3de57 #JavaScript #Tech',
-		'How to Hack Your Mornings so You Get Most of Your Time https://artplusmarketing.com/how-to-hack-your-mornings-so-you-get-most-of-your-time-b3e4d39c5b7c #LifeLessons #MorningThoughts',
-		'Setup Nodemon to auto restart #Nodejs #app server  https://hackernoon.com/setup-nodemon-to-auto-restart-nodejs-application-server-8d8993b7dfd9',
-		'Remove #Node Modules Recursively #nodejs #npm https://medium.com/@amanhimself/https-medium-com-amanhimself-remove-node-modules-recursively-5b5dd5fcc9ba',
-		'Introduction to Hybrid #Mobile #Apps https://hackernoon.com/introduction-to-hybrid-mobile-apps-c97720b30557 #ionic #IonicFramework #javascript',
-		'What is #Ionic? Learn about developing #mobile apps with #IonicFramework https://hackernoon.com/what-is-ionic-c1da6eab0d8a via @amanhimself #javascript',
-		'How to use Font Awesome in an #Ionic Application https://hackernoon.com/how-to-use-font-awesome-in-an-ionic-application-31638ed3f77d #javascript #mobile'
-	]
+  // NOTE: Tehcnical blog posts
+  tech: [
+    "Learn to build your own #Twitter #Bot in #Nodejs #JavaScript https://hackernoon.com/create-a-simple-twitter-bot-with-node-js-5b14eb006c08#.90wbpgb8e via @hackernoon #Tech",
+    "#Tutorial: Writing a #Nodejs #Twitter #Bot https://community.risingstack.com/node-js-twitter-bot-tutorial/ via @RisingStack #JavaScript #Tech ",
+    "#Tutorial: Writing an Advanced #Twitter #Bot using #Nodejs & #JavaScript https://community.risingstack.com/how-to-make-a-twitter-bot-with-node-js/ via @RisingStack #Tech",
+    "How to make a #Twitter #Bot with #Nodejs & #JavaScript https://chatbotslife.com/how-to-make-a-twitter-bot-with-nodejs-d5cb04fdbf97#.c4bzt4slr via @ChatBotsLife #Tech",
+    "Tip: How to use #Bower with #Heroku to manage your #FrontEnd #JavaScript Dependencies https://medium.com/@amanhimself/using-bower-with-heroku-cdc791320c88#.ria1rs44c #Tech",
+    "How #Nodejs works? An overview https://hackernoon.com/the-node-js-system-51090c35dddc #Tech",
+    "The #Nodejs way, #philosophy of the #JavaScript platform https://medium.com/@amanhimself/the-node-way-philosophy-of-a-platform-f9738ed5f9d2#.upgfhgvut #Tech",
+    "Migrating from #Expressjs 4 to 5 https://hackernoon.com/migrating-from-expressjs-4-to-5-156dcd80eb11#.blwzfpr42 via @hackernoon #JavaScript #Nodejs",
+    "How to Mock #Data for #Nodejs #Applications using #Faker.js https://hackernoon.com/how-to-mock-data-for-node-js-applications-using-faker-js-b1f4c0e78102 #JavaScript via @hackernoon",
+    "Getting Started with #IonicFramework https://hackernoon.com/getting-started-with-ionic-framework-an-overview-6725b687779b#.yvj6rnijh #Hybrid #overview #Mobile",
+    "How to use #Eslint in #Nodejs Applications? https://hackernoon.com/how-to-use-eslint-in-node-js-applications-cc4b2298ce55 via @hackernoon #JavaScript",
+    "Getting Started with #Sequelize for #Nodejs Applications https://hackernoon.com/getting-started-with-sequelize-for-nodejs-applications-2854c58ffb8c #JavaScript #MySQL",
+    "Tip: Setting up #MeanStack with #Webstorm https://medium.com/@amanhimself/setting-up-mean-stack-with-webstorm-5024d3c650ed #nodejs #javascript #development",
+    "How To Maintain #Sanity When Finding A #Job https://42hire.com/how-to-maintain-sanity-when-finding-a-job-87d368bcfd0c#.jintkr98n via @42hire #Career #Productivity #Tech #jobhunting #jobsearch",
+    "Converting a Buffer to JSON and Utf8 Strings in #Nodejs https://hackernoon.com/https-medium-com-amanhimself-converting-a-buffer-to-json-and-utf8-strings-in-nodejs-2150b1e3de57 #JavaScript #Tech",
+    "How to Hack Your Mornings so You Get Most of Your Time https://artplusmarketing.com/how-to-hack-your-mornings-so-you-get-most-of-your-time-b3e4d39c5b7c #LifeLessons #MorningThoughts",
+    "Setup Nodemon to auto restart #Nodejs #app server  https://hackernoon.com/setup-nodemon-to-auto-restart-nodejs-application-server-8d8993b7dfd9",
+    "Remove #Node Modules Recursively #nodejs #npm https://medium.com/@amanhimself/https-medium-com-amanhimself-remove-node-modules-recursively-5b5dd5fcc9ba",
+    "Introduction to Hybrid #Mobile #Apps https://hackernoon.com/introduction-to-hybrid-mobile-apps-c97720b30557 #ionic #IonicFramework #javascript",
+    "What is #Ionic? Learn about developing #mobile apps with #IonicFramework https://hackernoon.com/what-is-ionic-c1da6eab0d8a via @amanhimself #javascript",
+    "How to use Font Awesome in an #Ionic Application https://hackernoon.com/how-to-use-font-awesome-in-an-ionic-application-31638ed3f77d #javascript #mobile",
+    "Using Google Fonts in an #Ionic Application https://hackernoon.com/using-google-fonts-in-an-ionic-application-c3419c342f23 #JavaScript #Hybrid #IonicFramework #mobile",
+    "Basic Navigation in #Ionic Applications https://hackernoon.com/https-medium-com-amanhimself-basic-navigation-in-ionic-applications-ecb199cdf15b #JavaScript #JS #Hybrid #mbile via @hackernoon",
+    "How to Setup a React App from Scratch?” by @amanhimself https://codeburst.io/how-to-setup-a-react-app-from-scratch-860bad109837 #React #ReactJS #WebDev #JavaScript #JS"
+  ]
 };
