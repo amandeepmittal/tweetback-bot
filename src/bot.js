b3648981e8921bc5d4848a8004972fce2caf0e76
Aman Mittal <amandeepmittal@live.com>
2018-08-20 14:44:02 +0530

    mute stoic api

--- src/bot.js
+++ src/bot.js
@@ -1,33 +1,33 @@
-"use strict";
+'use strict';
 
 // Dependencies
-const config = require("../config/index");
-const Twit = require("twit");
-const stoicapi = require("stoic-api").random();
-const schedule = require("node-schedule");
-const ArticlesFromMedium = require("my-medium-articles-api");
+const config = require('../config/index');
+const Twit = require('twit');
+// const stoicapi = require("stoic-api").random();
+const schedule = require('node-schedule');
+const ArticlesFromMedium = require('my-medium-articles-api');
 
 const T = new Twit(config);
 
 // indicate the bot is running 👋
-console.log("Tweet-back Bot is runnning");
+console.log('Tweet-back Bot is runnning');
 
 /**
  * Tweet Medium Articles
  */
 
 function tweetMediumArticles() {
-  let Article = ArticlesFromMedium;
-  T.post("statuses/update", { status: Article }, (err, data, response) => {
-    if (err) {
-      console.error(
-        `========= ERROR! Cannot Send Tweet due to:\n${err}\n=========`
-      );
-    }
-    console.log(
-      `========= SUCCESS! Daily Medium Article Shared on Twitter =========`
-    );
-  });
+	let Article = ArticlesFromMedium;
+	T.post('statuses/update', { status: Article }, (err, data, response) => {
+		if (err) {
+			console.error(
+				`========= ERROR! Cannot Send Tweet due to:\n${err}\n=========`
+			);
+		}
+		console.log(
+			`========= SUCCESS! Daily Medium Article Shared on Twitter =========`
+		);
+	});
 }
 
 // tweet randomly and in every 24 hours
@@ -37,14 +37,14 @@ setInterval(tweetMediumArticles, 1000 * 60 * 60 * 24);
  * Tweet Daily Stoicism
  */
 
-function tweetDailyStoic() {
-  let message = stoicapi;
+// function tweetDailyStoic() {
+//   let message = stoicapi;
 
-  T.post("statuses/update", { status: message }, (err, data, response) => {
-    if (err) console.error("CANNOT SEND TWEET" + err);
-    console.log(`=== SUCCESS! Daily STOIC QUOTE Shared on Twitter ===`);
-  });
-}
+//   T.post("statuses/update", { status: message }, (err, data, response) => {
+//     if (err) console.error("CANNOT SEND TWEET" + err);
+//     console.log(`=== SUCCESS! Daily STOIC QUOTE Shared on Twitter ===`);
+//   });
+// }
 
 // tweet randomly and in every 24 hours
-setInterval(tweetDailyStoic, 1000 * 60 * 60 * 24);
+// setInterval(tweetDailyStoic, 1000 * 60 * 60 * 24);
