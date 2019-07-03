4a0902fb9b1f438500b7df94c061946415e3e653
Aman Mittal <amandeepmittal@live.com>
2019-07-03 06:02:43 +0530

    update deps

--- src/bot.js
+++ src/bot.js
@@ -1,38 +1,34 @@
-'use strict';
+'use strict'
 
 // Dependencies
-const config = require('../config/index');
-const Twit = require('twit');
+const config = require('../config/index')
+const Twit = require('twit')
 // const stoicapi = require("stoic-api").random();
-const schedule = require('node-schedule');
-const ArticlesFromMedium = require('my-medium-articles-api');
+const schedule = require('node-schedule')
+const ArticlesFromMedium = require('my-medium-articles-api')
 
-const T = new Twit(config);
+const T = new Twit(config)
 
 // indicate the bot is running 👋
-console.log('Tweet-back Bot is runnning');
+console.log('Tweet-back Bot is runnning')
 
 /**
  * Tweet Medium Articles
  */
 
 function tweetMediumArticles() {
-	let Article = ArticlesFromMedium;
+	let Article = ArticlesFromMedium
 	T.post('statuses/update', { status: Article }, (err, data, response) => {
 		if (err) {
-			console.error(
-				`========= ERROR! Cannot Send Tweet due to:\n${err}\n=========`
-			);
+			console.error(`========= ERROR! Cannot Send Tweet due to:\n${err}\n=========`)
 		}
-		console.log(
-			`========= SUCCESS! Daily Medium Article Shared on Twitter =========`
-		);
-	});
+		console.log(`========= SUCCESS! Daily Medium Article Shared on Twitter =========`)
+	})
 }
 
 // tweet randomly and in every 24 hours
-setInterval(tweetMediumArticles, 1000 * 60 * 60 * 24);
-// tweetMediumArticles()
+setInterval(tweetMediumArticles, 1000 * 60 * 60 * 24)
+tweetMediumArticles()
 
 /**
  * Tweet Daily Stoicism
