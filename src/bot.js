6447e855e5219d96fa1973fe356f8d3316bb2852
Aman Mittal <amandeepmittal@live.com>
2020-08-05 16:57:39 +0530

    update API

--- src/bot.js
+++ src/bot.js
@@ -1,47 +1,34 @@
-'use strict'
+'use strict';
 
 // Dependencies
-const config = require('../config/index')
-const Twit = require('twit')
-// const stoicapi = require("stoic-api").random();
-const schedule = require('node-schedule')
-const ArticlesFromMedium = require('my-medium-articles-api')
+const config = require('../config/index');
+const Twit = require('twit');
+const schedule = require('node-schedule');
+const ArticlesFromMedium = require('my-medium-articles-api');
 
-const T = new Twit(config)
+const T = new Twit(config);
 
 // indicate the bot is running 👋
-console.log('Tweet-back Bot is runnning')
+console.log('Tweet-back Bot is running');
 
 /**
  * Tweet Medium Articles
  */
 
 function tweetMediumArticles() {
-	let Article = ArticlesFromMedium
-	T.post('statuses/update', { status: Article }, (err, data, response) => {
-		if (err) {
-			console.error(`========= ERROR! Cannot Send Tweet due to:\n${err}\n=========`)
-		}
-		console.log(`========= SUCCESS! Daily Medium Article Shared on Twitter =========`)
-	})
+  let Article = ArticlesFromMedium;
+  T.post('statuses/update', { status: Article }, (err, data, response) => {
+    if (err) {
+      console.error(
+        `========= ERROR! Cannot Send Tweet due to:\n${err}\n=========`
+      );
+    }
+    console.log(
+      `========= SUCCESS! Daily Medium Article Shared on Twitter =========`
+    );
+  });
 }
 
 // tweet randomly and in every 24 hours
-setInterval(tweetMediumArticles, 1000 * 60 * 60 * 24)
-// tweetMediumArticles()
-
-/**
- * Tweet Daily Stoicism
- */
-
-// function tweetDailyStoic() {
-//   let message = stoicapi;
-
-//   T.post("statuses/update", { status: message }, (err, data, response) => {
-//     if (err) console.error("CANNOT SEND TWEET" + err);
-//     console.log(`=== SUCCESS! Daily STOIC QUOTE Shared on Twitter ===`);
-//   });
-// }
-
-// tweet randomly and in every 24 hours
-// setInterval(tweetDailyStoic, 1000 * 60 * 60 * 24);
+setInterval(tweetMediumArticles, 1000 * 60 * 60 * 20);
+// tweetMediumArticles();
